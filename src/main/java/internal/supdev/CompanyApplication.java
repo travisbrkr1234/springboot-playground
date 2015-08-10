6570811bad44863758f81bc627e2f475b480e1d1
Jeremiah Marks <jeremiah@jlmarks.org>
2015-08-10 16:40:36 -0700

    Move migrationApp.java into CompanyApplication.java.  Runs, but no css.

--- src/main/java/internal/supdev/CompanyApplication.java
+++ src/main/java/internal/supdev/CompanyApplication.java
@@ -1,21 +1,78 @@
 package main.java.internal.supdev;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Date;
+import java.util.Map;
+
 /**
  * Created by carlos.ochoa on 7/31/2015.
  */
-
-
-@SpringBootApplication
-@Configuration
-@ComponentScan
 @EnableAutoConfiguration
-public class CompanyApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CompanyApplication.class, args);
+@ComponentScan
+@Controller
+public class CompanyApplication extends WebMvcConfigurerAdapter {
+
+    @RequestMapping("/")
+    public String home(Map<String, Object> model) {
+        model.put("message", "Hello World");
+        model.put("title", "Hello Home");
+        model.put("date", new Date());
+        return "home";
     }
+
+    @RequestMapping("/foo")
+    public String foo() {
+        throw new RuntimeException("Expected exception in controller");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/wizardoptions").setViewName("wizardoptions");
+        registry.addViewController("/export").setViewName("export");
+    }
+
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplicationBuilder(CompanyApplication.class).run(args);
+    }
+
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private SecurityProperties security;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
+                    .fullyAuthenticated().and().formLogin().loginPage("/login")
+                    .failureUrl("/login?error").permitAll().and().logout().permitAll();
+        }
+
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        }
+
+    }
+
 }
