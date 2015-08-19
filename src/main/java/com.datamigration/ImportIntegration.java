a6b7c2c9fcd7f58a88684039ed4eff7a9f4cc052
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-08-19 02:38:02 -0700

    Added the base of function that will check if the custom field has been created and then will create it. NOTE: it is just the base, but it will compile and run.

--- src/main/java/com.datamigration/ImportIntegration.java
+++ src/main/java/com.datamigration/ImportIntegration.java
@@ -1,3 +1,17 @@
+/*
+* @Author: Jeremiah Marks
+* @Date:   2015-08-18 20:36:11
+* @Last Modified 2015-08-19
+* @Last Modified time: 2015-08-19 02:09:47
+*/
+
+/*"Plans for the night"
+18August2015:
+    Check if 'FK' exists as a custom field.
+    Create if it does not.
+    Do search for contactID and FK
+    Open file
+*/
 package com.datamigration;
 
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
@@ -14,7 +28,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Arrays;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -24,20 +38,34 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ImportIntegration {
 
-    private String appName = null;
-    private String apiKey = null;
-    private String name;
-    private MultipartFile file;
+    private ArrayList defaultTemplate;
+    /*  This will hold the default data for an API call*/
+
+    /*      I wonder how feasiable it would be to just have a
+            series of defaulttemplates available to make needed
+            calls from.  Seems like the right thing to do.
+    */
+    private ArrayList dataToPass;
+    private boolean connected = false;
+    /*This tracks whether the app name and apikey have been validated*/
     private boolean fileuploaded;
+    private HashMap submitData;
+    private int numExistingContacts=0;
+    private int results;
+    private MultipartFile file;
+    private String apiKey = null;
+    private String appName = null;
+    private String uniqueIDField = null;
+    /*These are set to null so that I can use them to display
+    appropriate areas of the site.*/
+    private String name;
+    private URL serverAddress;
     private XmlRpcClient connection;
     private XmlRpcClientConfigImpl config;
-    private URL serverAddress;
-    private ArrayList dataToPass;
-    private HashMap submitData;
-    private int results;
-    private boolean connected = false;
-    private int numExistingContacts=0;
-          
+    private ArrayList customFields;
+
+
+
     public String getAppName() {
         return appName;
     }
@@ -45,30 +73,32 @@ public class ImportIntegration {
         this.appName = appName;
     }
 
-
     public String getApiKey() {
         return apiKey;
     }
     public void setApiKey(String apiKey) {
         this.apiKey = apiKey;
     }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public void setFile(MultipartFile file){
             try {
                 byte[] bytes = file.getBytes();
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name)));
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(this.name)));
                 stream.write(bytes);
                 stream.close();
                 this.fileuploaded=true;
             } catch (Exception e) {
                 this.fileuploaded=false;
             }
-        }
+    }
+
     public void createConnection() throws MalformedURLException{
         try{
             this.serverAddress = new URL("https://" + this.appName + ".infusionsoft.com:443/api/xmlrpc");
@@ -81,6 +111,7 @@ public class ImportIntegration {
             this.connected=false;
         }
     }
+
     public void connect() throws XmlRpcException, MalformedURLException{
         this.submitData= new HashMap();
         this.connected=false;
@@ -96,23 +127,53 @@ public class ImportIntegration {
           this.connected=false;
       }
     }
+
     public int getNumExistingContacts() throws XmlRpcException{
+        int numofRecords=-1;
+        try{
+            numofRecords=(int)this.getRecordsOnTable("Contact");
+        }catch(XmlRpcException e){}
+        return numofRecords;
+    }
+    public int getRecordsOnTable(String tableName) throws XmlRpcException{
+        int records=-1;
         this.submitData= new HashMap();
         this.dataToPass = new ArrayList();
         this.dataToPass.add(this.apiKey);
-        this.dataToPass.add("Contact");
+        this.dataToPass.add(tableName);
         this.dataToPass.add(this.submitData);
         if (this.connected){
             try {
-                this.numExistingContacts = (int) this.connection.execute("DataService.count", this.dataToPass);
+                records = (int) this.connection.execute("DataService.count", this.dataToPass);
             }catch (XmlRpcException e){}
         }
-        return this.numExistingContacts;
+        return records;
     }
+
     public boolean getConnected(){
         try{
             this.connect();
         }catch(XmlRpcException|MalformedURLException e){}
         return this.connected;
     }
+
+    public boolean checkForCustomField(String fieldName){
+        /*This should probably be set up in such a way*/
+        /*that is has some sort of @ decarator to help*/
+        /*ensure it can only be ran after it connected*/
+        this.dataToPass = new ArrayList();
+        this.dataToPass.add(this.apiKey);
+        this.dataToPass.add("DataFormField");
+        this.dataToPass.add(this.submitData);
+        try{
+            this.connection.execute("DataService.count", this.dataToPass);
+        } catch(XmlRpcException e){}
+        finally{
+            return this.connected;
+            /*Note: this is only here for testing purpose.  It
+            is actually stupud to have this here.
+            */
+        }
+    }
+
 }
