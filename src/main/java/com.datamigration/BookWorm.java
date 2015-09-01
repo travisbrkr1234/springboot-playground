81c8fad2983822fa3b20c481628f7cd289f59ca3
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-09-01 12:27:14 -0700

    added simple file reader

--- src/main/java/com.datamigration/BookWorm.java
+++ src/main/java/com.datamigration/BookWorm.java
@@ -1,15 +1,40 @@
-package com.datamigration;
+//package com.datamigration;
+
+import java.io.*;
 
 /**
  * Created by carlos.ochoa on 8/28/2015.
  */
-public class BookWorm {
+public class Bookworm {
     //File Reading class
-    public String(privateKey, contactID, cardID, planID, productIDs, subscriptionIDs, processSpecials, promoCodes, leadAffiliateID, saleAffiliateID) {
+    public static void main(String[] args) throws IOException {
+        String myFile = "C:\\Users\\carlos.ochoa\\Desktop\\order.csv";
+        String splitBy = ",";
+        BufferedReader br = new BufferedReader(new FileReader(myFile));
+        String line = br.readLine();
+        while((line = br.readLine()) !=null) {
+            String[] b = line.split(splitBy);
+            System.out.println("id: " + b[0]);
+            System.out.println("email: " + b[1]);
+            System.out.println("total: " + b[2]);
+            System.out.println(b[3]);
+            System.out.println(b[4]);
+            System.out.println(b[5]);
+            System.out.println(b[6]);
+            System.out.println(b[7]);
+            System.out.println(b[8]);
+            System.out.println(b[9]);
+            System.out.println(b[10]);
+            //code to import data from template
+        }
+        br.close();
     }
-    //Use properties file to form these
-    //
-    //
-    //
 }
+
+//public String(privateKey, contactID, cardID, planID, productIDs, subscriptionIDs, processSpecials, promoCodes, leadAffiliateID, saleAffiliateID) {
+//}
+//Use properties file to form these
+//
+//
+//
 
